cd2b574342b539c1ad0762dd788ec0aa0a161feb
billowbashir <billowbashir@gmail.com>
2018-08-13 21:11:51 +0300

    complete quote application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent } from './quotes/quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
+import { TimePassedPipe } from './time-passed.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuoteComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent,
+    QuoteVoteComponent,
+    TimePassedPipe,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quotes';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
-})
-export class QuotesComponent implements OnInit {
+})export class QuoteComponent implements OnInit {
+  quotes = [
+      new Quote(1,'some guy','when i was born i was so shocked that i didnt speak for one and a half years',new Date(2018,6,16) ),
+      new Quote(2,'shakespeare','to be or not to be, that is the question', new Date(1968,7,5) ),
+
+
+  ]
+
+    toogleDetails(index){
+        this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    }
+
+    completeQuote(isComplete,index){
+       if (isComplete) {
+           this.quotes.splice(index, 1);
+           }
+           }
 
   constructor() { }
-
   ngOnInit() {
   }
+  deleteQuote(isComplete,index){
+        if (isComplete){
+            let toDelete=confirm(`confirm you want to delete ${this.quotes[index].name}`)
 
+            if(toDelete){
+                this.quotes.splice(index,1)
+            }
+        }
+    }
+addNewQuote(quote){
+  let quoteLength = this.quotes.length;
+  quote.id = quoteLength + 1;
+  quote.completeDate = new Date(quote.completeDate)
+  this.quotes.push(quote)
+
+}
 }
